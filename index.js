d3c2e75025b1828c7ac0df4820ae42ed12f84fe0
realbits-lab <dev.realbits@gmail.com>
2023-02-08 15:22:33 +0900

    Print console logs.

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
   .readdirSync(commandsPath)
   .filter((file) => file.endsWith(".js"));
+console.log("commandFiles:", commandFiles);
 
 for (const file of commandFiles) {
+  console.log("file: ", file);
   const filePath = path.join(commandsPath, file);
   const command = require(filePath);
 
@@ -32,8 +34,10 @@ const eventsPath = path.join(__dirname, "events");
 const eventFiles = fs
   .readdirSync(eventsPath)
   .filter((file) => file.endsWith(".js"));
+console.log("eventFiles:", eventFiles);
 
 for (const file of eventFiles) {
+  console.log("file: ", file);
   const filePath = path.join(eventsPath, file);
   const event = require(filePath);
   // console.log("event: ", event);
