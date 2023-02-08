f66d8ef36b28f6b08ea1af987c2433444f407e52
realbits-lab <dev.realbits@gmail.com>
2023-02-08 12:51:00 +0900

    Remove unused env variables.

--- commands/draw.js
+++ commands/draw.js
@@ -28,6 +28,7 @@ module.exports = {
     const DOWNLOAD_DIRECTORY = "../download_files/";
     const MINT_PAGE_URL = process.env.MINT_PAGE_URL;
     const API_POST_URL = process.env.API_POST_URL;
+    const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
     console.log("process.env.MINT_PAGE_URL: ", process.env.MINT_PAGE_URL);
     console.log("process.env.API_POST_URL: ", process.env.API_POST_URL);
 
@@ -121,7 +122,11 @@ module.exports = {
               Accept: "application/json",
               "Content-Type": "application/json",
             },
-            body: JSON.stringify({ prompt: prompt, imageUrl: imageUrl }),
+            body: JSON.stringify({
+              prompt: prompt,
+              imageUrl: imageUrl,
+              discordBotToken: DISCORD_BOT_TOKEN,
+            }),
           });
           console.log("imageFetchResponse.status: ", imageFetchResponse.status);
           console.log(
